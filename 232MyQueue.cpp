bbba5d2acb4a8b5523bb8fc97706925f160d33fc
DGREY <2112166064@qq.com>
2025-07-15 19:19:09 +0800

    0715

---
+++ 232MyQueue.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+// class MyQueue {
+//     stack<int> in, out;
+// public:
+//     void push(int x) { in.push(x); }
+
+//     int pop() {
+//         if (out.empty()) while (!in.empty()) out.push(in.top()), in.pop();
+//         int x = out.top(); out.pop();
+//         return x;
+//     }
+
+//     int peek() {
+//         if (out.empty()) while (!in.empty()) out.push(in.top()), in.pop();
+//         return out.top();
+//     }
+
+//     bool empty() { return in.empty() && out.empty(); }
+// };
